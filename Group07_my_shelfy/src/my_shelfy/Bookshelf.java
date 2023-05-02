2d14b9a7224b79e742d2d26c85efc4f963047c37
pcasanovaa <127229225+pcasanovaa@users.noreply.github.com>
2023-05-02 17:27:29 +0200

    create bookshelf class
    Co-Authored-By: ivancaccamo <126600539+ivancaccamo@users.noreply.github.com>

---
+++ Group07_my_shelfy/src/my_shelfy/Bookshelf.java
@@ -0,0 +1,18 @@
+package my_shelfy;
+
+public class Bookshelf {
+	private int rows=5;
+	private int columns=6;
+	 private
+	    Bookshelf[][] schema_matrice = new Bookshelf[rows][columns];
+	
+	public Bookshelf() // Bookshelf constructor, initializing private rows and columns
+	{
+		for (int i = 0; i < rows; ++i)
+			for (int j = 0; j < columns; ++j)
+				schema_matrice[i][j] = new Bookshelf();
+	}
+	
+	private void 
+
+}
