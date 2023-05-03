2379f2439d698c3570620a12b65802c3f4d47e27
pcasanovaa <127229225+pcasanovaa@users.noreply.github.com>
2023-05-03 10:34:53 +0200

    uso Convenzioni
    sistemazione del nome della variabile nella classe Bookshelf

--- Group07_my_shelfy/src/my_shelfy/Bookshelf.java
+++ Group07_my_shelfy/src/my_shelfy/Bookshelf.java
@@ -4,13 +4,13 @@ public class Bookshelf {
 	private int rows=6;
 	private int columns=5;
 	 private
-	    Bookshelf[][] schema_matrice = new Bookshelf[rows][columns];
+	    Bookshelf[][] schemaMatrice = new Bookshelf[rows][columns];
 	
 	public Bookshelf() // Bookshelf constructor, initializing private rows and columns
 	{
 		for (int i = 0; i < rows; ++i)
 			for (int j = 0; j < columns; ++j)
-				schema_matrice[i][j] = new Bookshelf();
+				schemaMatrice[i][j] = new Bookshelf();
 	}
 	
 	private void 
