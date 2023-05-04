9e46d37e2a3901c2a0e75ac1ab0863966aee71dd
ivancaccamo <126600539+ivancaccamo@users.noreply.github.com>
2023-05-04 11:55:25 +0200

    Creazione metodo countEmptyspace della Bookshelf
    Co-Authored-By: pcasanovaa <127229225+pcasanovaa@users.noreply.github.com>

--- Group07_my_shelfy/src/my_shelfy/Bookshelf.java
+++ Group07_my_shelfy/src/my_shelfy/Bookshelf.java
@@ -2,8 +2,8 @@ package my_shelfy;
 import java.util.*;
 
 public class Bookshelf {
-	private int rows=6;
-	private int columns=5;
+	private int rows=5;
+	private int columns=4;
 	 private
 	    Bookshelf[][] schemaMatrice = new Bookshelf[rows][columns];
 	
@@ -14,21 +14,63 @@ public class Bookshelf {
 				schemaMatrice[i][j] = new Bookshelf();
 	}
 	
-	private int getColumns(){
+	public int getColumns(){
 		int colSel;
 		do {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc= new Scanner(System.in);
 		System.out.println("In che colonna vuoi inserire le carte? (compreso tra 1 e 6)");
 		colSel=sc.nextInt();// per colSel s'intende la colonna selezionata dal giocatore
 		
-		if(colSel<1 && colSel>6) {
+		if(colSel<rows && colSel>columns) {
 			System.out.println("Colonna inesistente, seleziona una colonna giusta");
 		}
-		}while(colSel<1 && colSel>6);
+		}while(colSel<rows && colSel>columns);
 		
-		
-		
+		return colSel;	
+	}
 	
+	public static int countEmptyspace (Bookshelf[][] schemaMatrice, int colSel) {
+		int emptySpaces=0;
+		
+		for(int i=0; i<schemaMatrice.length; i++) {
+			if (schemaMatrice[i][colSel]== null) {
+				emptySpaces++;
+			}
+		}
+		System.out.println("La colonna ha"+emptySpaces+" spazi vuoti");
+		return emptySpaces;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+    /*
+	public static List<int[]> getFreePositions(Bookshelf[][] schemaMatrice){
+		List<int[]> freePositions = new ArrayList<>();
+		
+		for(int i=0; i< rows; i-- ) {
+			
+			
+		}
 	}
 
+	*/
+	
+	
+	
+	
+	/*
+	public static void fillColumns(Bookshelf[][] schemaMatrice , int colSel ) {
+		
+		for(int i=0; i<rows; i++) {
+			if(rows[i])
+			schemaMatrice[i][colSel]
+		}
+	}
+	*/
+	
 }
