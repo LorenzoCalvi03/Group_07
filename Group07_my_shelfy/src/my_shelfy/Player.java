1b12f38d4e7d754d69eae9cc157bcd65d38ea2f9
LorenzoCalvi03 <l.calvi3@studenti.unibg.it>
2023-05-16 11:37:27 +0200

    caricamento classe player
    creazione classe player

--- Group07_my_shelfy/src/my_shelfy/Player.java
+++ Group07_my_shelfy/src/my_shelfy/Player.java
@@ -2,5 +2,30 @@ package my_shelfy;
 
 public class Player 
 {
-	int points = 0;	
+	 private int points ;	
+	 public static int  COUNTER;
+	 //private Bookshelf shelf;
+	 //private Personalgoal personal;
+	 
+	 public Player() {
+		 if(COUNTER<4) {
+			 COUNTER++;
+		 }
+		 	this.points=0;
+	 }
+	 
+	 public void setPoint(int point) {
+		this.points=point; 
+	 }
+	 
+	 public int getPoint() {
+		 return this.points;
+	 }
+	/*  public void incrementaPunti() {
+		  int point=this.points
+		// point=personal.getpoint;;
+				  this.points=
+		  
+	  }*/
+	 
 }
