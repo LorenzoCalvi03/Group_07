aed8837f3cc4b1ee4d35fdfd8e58d7625b388354
pcasanovaa <127229225+pcasanovaa@users.noreply.github.com>
2023-05-29 12:22:10 +0200

    estrazione casuale primo player primo turno nella classe Main

--- Group07_my_shelfy/src/my_shelfie/Main.java
+++ Group07_my_shelfy/src/my_shelfie/Main.java
@@ -25,26 +25,26 @@ public class Main {
 		board.fill();
 		board.printBoard();
 		System.out.println("Common goal 1: "+ GameMaster.g.commongoal1.toString());
-		System.out.println("Common goal 1: "+ GameMaster.g.commongoal2.toString());
-		System.out.println("Immettere nome utente1: ");
-		Player player1 =new Player(sc.nextLine());
-		
-		System.out.println("Immettere nome utente2: ");
-		Player player2 =new Player(sc.nextLine());
-		
-		if (numPlayer == 3) {
-			System.out.println("Immettere nome utente3: ");
-		    Player player3 = new Player(sc.nextLine());
-		}else if (numPlayer == 4) {
-			System.out.println("Immettere nome utente3: ");
-		    Player player3 = new Player(sc.nextLine());
-		    System.out.println("Immettere nome utente4: ");
-		    Player player4 = new Player(sc.nextLine());
-		}
+		System.out.println("Common goal 2: "+ GameMaster.g.commongoal2.toString());
+		List<Player> players = new ArrayList<>();
 
-		System.out.println("Primo turno: ");
+	    for (int i = 1; i <= numPlayer; i++) {
+	        System.out.println("Immettere nome utente" + i + ": ");
+	        Player player = new Player(sc.nextLine());
+	        players.add(player);
+	    }
 
+	    // Genera l'ordine casuale dei giocatori
+	    Collections.shuffle(players);
+
+	    System.out.println("Ordine casuale dei giocatori: ");
+	    for (int i = 0; i < players.size(); i++) {
+	        System.out.println("Giocatore " + (i + 1) + ": " + players.get(i).getUsername());
+	    }
+
+	    System.out.println("Primo turno: ");
 	}
+
 }
 
 
