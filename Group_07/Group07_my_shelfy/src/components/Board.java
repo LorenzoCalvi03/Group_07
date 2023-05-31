8ae1ddb6ff8688ac0f7758f99b8a6f6374cd1831
filippobonfanti <f.bonfanti5@studenti.unibg.it>
2023-05-31 11:24:07 +0200

    sistemazione board

--- Group_07/Group07_my_shelfy/src/components/Board.java
+++ Group_07/Group07_my_shelfy/src/components/Board.java
@@ -46,7 +46,7 @@ public   class  Board {
 		if (this.playGround[x][y] != null) {
 			if (((x > 0 && this.playGround[x - 1][y].getType()==null)
 					|| (x < GameMaster.g.getRows() && this.playGround[x + 1][y].getType()==null))
-					|| ((y > 0 && this.playGround[x][y - 1] == null)
+					|| ((y > 0 && this.playGround[x][y - 1].getType() ==null)
 					|| (y < GameMaster.g.getCols() && this.playGround[x][y + 1].getType()==null))) {
 				return true;
 			} else {
@@ -61,7 +61,10 @@ public   class  Board {
 		int counter = 0; // conta qunate tessere con altre tessere adiacenti ci sono
 		for (int i = 0; i < GameMaster.g.getRows(); i++) {
 			for (int j = 0; j < GameMaster.g.getCols(); j++) {
-				if(puoPescare(i, j)) {
+				if (((i > 0 && this.playGround[i - 1][j].getType()!=null)
+						|| (i < GameMaster.g.getRows()-1 && this.playGround[i + 1][j].getType()!=null))
+						|| ((j > 0 && this.playGround[i][j - 1].getType()!= null)
+						|| (j < GameMaster.g.getCols()-1 && this.playGround[i][j + 1].getType()!=null))) {
 					counter++;
 				}
 			}
